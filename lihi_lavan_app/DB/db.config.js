68866b7036611ad038b77dd921b3a41050de03e6
LIHILA <85398922+LIHILA@users.noreply.github.com>
2021-09-27 18:26:25 +0300

    HW 7

---
+++ lihi_lavan_app/DB/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "yT3mrpev@" ,
+    DB: "mysql"
+    };
---
+++ lihi_lavan_app/public/javascripts/myFirstJavaScript.js
@@ -0,0 +1,5 @@
+document.getElementsByID("Lihiname").innherHTML="MY FIRST JAVA SCRIPT";
+
+function changeTitle(){
+    document.getElementById("lihi123").innerHTML = "Hello Guest"
+}   
